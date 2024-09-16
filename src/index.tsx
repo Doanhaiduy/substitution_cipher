48a8a544d79488b41f11f7f78eb8a980377f92e6
Doanhaiduy <haiduytbt2k3@gmail.com>
2024-09-17 00:05:58 +0700

    enable vercel analytics

--- src/index.tsx
+++ src/index.tsx
@@ -4,11 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { DataReplacementProvider } from './contexts/DataReplacementContext';
+import { Analytics } from '@vercel/analytics/react';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
+
 root.render(
     <React.StrictMode>
         <DataReplacementProvider>
+            <Analytics />
             <App />
         </DataReplacementProvider>
     </React.StrictMode>
